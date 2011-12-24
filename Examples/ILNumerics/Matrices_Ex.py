a9b010436c3b3f0d450d7091204c66e25a5ff083
joe.moorhouse <joe.moorhouse@c8c2ea74-c23b-f8f7-fa78-4824bd2ed850>
2011-12-24 21:01:17 +0000

    ILNumerics support deprecated.
    git-svn-id: http://ironlab.googlecode.com/svn/trunk@35 c8c2ea74-c23b-f8f7-fa78-4824bd2ed850

--- Examples/ILNumerics/Matrices_Ex.py
+++
@@ -1,7 +0,0 @@
-from ilnumerics import *
-# Find (complex) eigenvalues:
-print eig(array([[1., -1.], [1., 1.]]))
-
-# Matrix multiplication ('*' operator does element-wise multiplication)
-# Calls MKL library if available, so should be fast!
-print multiply(counter(1000, 1500), counter(1500, 1000))
