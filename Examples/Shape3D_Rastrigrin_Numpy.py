d915231cfd5774e883cf1b4e18d6f008e6ea6e03
SteveUM <3000.sjg@gmail.com>
2016-09-17 12:33:03 -0400

    Add files via upload

---
+++ Examples/Shape3D_Rastrigrin_Numpy.py
@@ -0,0 +1,9 @@
+﻿import numpy as np
+import ironplot as ip
+
+# plot Rastrigrin function
+x1 = np.arange(-5.12, 5.12, 0.1024)
+x2 = np.arange(-5.12, 5.12, 0.1024)
+X1, X2 = np.meshgrid(x1, x1)
+Z = (X1**2 - 10 * np.cos(2 * np.pi * X1)) + (X2**2 - 10 * np.cos(2 * np.pi * X2)) + 20
+ip.plot3d(x1, x1, Z)
