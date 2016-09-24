5834c176a2a9add36c8d4fa4c86229730274e640
SteveUM <3000.sjg@gmail.com>
2016-09-24 15:09:34 -0400

    Changed order of import to allow auto-install of numpy.

---
+++ Shape3D_Rastrigrin_Numpy.py
@@ -0,0 +1,9 @@
+﻿import ironplot as ip
+import numpy as np
+
+# plot Rastrigrin function
+x1 = np.arange(-5.12, 5.12, 0.1024)
+x2 = np.arange(-5.12, 5.12, 0.1024)
+X1, X2 = np.meshgrid(x1, x1)
+Z = (X1**2 - 10 * np.cos(2 * np.pi * X1)) + (X2**2 - 10 * np.cos(2 * np.pi * X2)) + 20
+ip.plot3d(x1, x1, Z)
